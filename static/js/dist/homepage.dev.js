70f78775ea2a48d38a204c0ee2f2f08acf680209
roger0426 <roger5521610@gmail.com>
2021-08-26 14:43:36 +0800

    create homepage, cam page basic structure

---
+++ static/js/dist/homepage.dev.js
@@ -0,0 +1,13 @@
+"use strict";
+
+$(document).ready(function () {
+  console.log('loaded');
+  $("#camera-btn").click(function () {
+    $("#home-container").hide();
+    $("#camera-container").show();
+  });
+  $("#camera-container").click(function () {
+    $("#home-container").show();
+    $("#camera-container").hide();
+  });
+});
