c60515a620bfd63be9e575aff69818f91f35f827
Tzu1999 <Yukage762@gmail.com>
2021-08-30 13:31:26 +0800

    modify sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const staticCacheName = 'site-static';
+const staticCacheName = 'site-static-v1';
+const dynamicCache = 'site-dynamic-v1';
 const assets = ['/', '/templates/dist/homepage.html', '/static/js/app.js',
     '/static/js/homepage.js', '/static/js/dist/homepage.dev.js', '/static/sass/dist/homepage.css', '/static/sass/homepage.sass',
     '/static/file/camera.png', '/static/file/cross-icon.png', '/static/file/favorite.png', '/static/file/history.png',
     '/static/file/previous-icon-b.png', '/static/file/previous-icon-w.png'];
+
 self.addEventListener('install', evt => {
     //console.log('service worker has been installed');
     evt.waitUntil(
@@ -14,12 +16,26 @@ self.addEventListener('install', evt => {
 });
 self.addEventListener('activate', evt => {
     //console.log('service worker has been activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName && key !== dynamicCache)
+                .map(key => caches.delete(key))
+            )
+        })
+    );
 });
 self.addEventListener('fetch', evt => {
     //console.log('fetch event', evt);
+    /*
     evt.respondWith(
         caches.match(evt.request).then(cacheRes => {
-            return cacheRes || fetch(evt.request);
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(dynamicCache).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    return fetchRes;
+                })
+            });
         })
-    );
+    );*/
 });
