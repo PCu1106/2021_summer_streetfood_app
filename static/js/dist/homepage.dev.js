fa43d7b62a66125df62dc44834ec68a19ccc6ae6
morning <joeroy5376998@gmail.com>
2021-09-11 11:31:16 +0800

    add camera snapshot

--- static/js/dist/homepage.dev.js
+++ static/js/dist/homepage.dev.js
@@ -4,27 +4,57 @@ $(document).ready(function () {
   console.log('loaded');
   var constraints = {
     video: {
-      facingMode: "user"
+      facingMode: "environment"
     },
     audio: false
-  }; //"user" just for easier testing on PC, should be "environment"
-
+  };
   var cameraWindow = document.querySelector("#cam-window"),
-      cameraSensor = document.querySelector("#cam-sensor");
+      cameraSensor = document.querySelector("#cam-sensor"),
+      cameraOutput = document.querySelector("#cam-output");
 
   function cameraStart() {
     console.log('camera start');
     navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
-      track = stream.getTracks()[0];
+      Window.stream = stream;
       cameraWindow.srcObject = stream;
+      cameraWindow.play();
+      console.log(Window.stream); //show MediaStream info
     })["catch"](function (error) {
       console.error('something is broken.', error);
     });
   }
 
+  function cameraStop() {
+    if (cameraWindow.srcObject != null) {
+      console.log('camera stop');
+      var videoStream = cameraWindow.srcObject.getTracks();
+
+      if (videoStream != null) {
+        videoStream.forEach(function (stream) {
+          stream.stop();
+        });
+        cameraWindow.srcObject = null;
+      }
+    }
+  }
+
+  function cameraSnapshot() {
+    console.log('take picture');
+    cameraOutput.style.display = "block";
+    cameraSensor.width = cameraWindow.videoWidth;
+    cameraSensor.height = cameraWindow.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraWindow, 0, 0, cameraSensor.width, cameraSensor.height);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+  }
+
   $("#camera-btn").click(function () {
     $("#home-container").hide();
     $("#camera-container").show();
+    $("#shoot-btn").show();
+    $("#shoot-again-btn").hide();
+    $("#goto-list-btn").hide();
+    cameraOutput.style.display = "none";
     cameraStart();
   });
   $("#favorite-btn").click(function () {
@@ -43,10 +73,22 @@ $(document).ready(function () {
     $("#history-container").hide();
   });
   $("#camera-exit").click(function () {
-    console.log('camera stop');
-    cameraWindow.srcObject.getTracks().forEach(function (track) {
-      track.stop();
-    });
+    cameraStop();
+  });
+  $("#shoot-btn").click(function () {
+    cameraSnapshot();
+    cameraStop();
+    $("#shoot-btn").hide();
+    $("#shoot-again-btn").show();
+    $("#goto-list-btn").show();
+  });
+  $("#shoot-again-btn").click(function () {
+    $("#camera-container").show();
+    $("#shoot-btn").show();
+    $("#shoot-again-btn").hide();
+    $("#goto-list-btn").hide();
+    cameraOutput.style.display = "none";
+    cameraStart();
   });
   var intervalId = window.setInterval(function () {
     // check every 10 seconds
