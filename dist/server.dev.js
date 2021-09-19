ed5541945bd24248d1640229df8819665efa1b5c
morning <joeroy5376998@gmail.com>
2021-09-19 14:18:14 +0800

    modify server for https service

--- dist/server.dev.js
+++ dist/server.dev.js
@@ -1,19 +1,36 @@
 "use strict";
 
+var fs = require('fs');
+
+var https = require('https');
+
+var path = require('path');
+
 var express = require('express');
 
+var keyPath = '../ssl/private.key';
+var certPath = '../ssl/certificate.pem';
+var hskey = fs.readFileSync(keyPath);
+var hscert = fs.readFileSync(certPath);
 var app = express();
+var port = 8787; //var credentials = { key:hskey, cert:hscert};
 
-var path = require("path");
-
-var hostname = '127.0.0.1';
-var port = 5000; // release the limit of '/static' file
-
-app.use('/static', express["static"](__dirname + '/static'));
+app.use('./static', express["static"](__dirname + './static'));
+/*
 app.get('/', function (req, res) {
-  console.log("".concat(__dirname + '/static'));
+  console.log(`${__dirname+'/static'}`);
   res.sendFile(path.join(__dirname + '/templates/dist/homepage.html'));
 });
-app.listen(port, hostname, function () {
-  console.log("Server running at http://".concat(hostname, ":").concat(port, "/"));
+
+app.listen(port, "127.0.0.1", () => {
+  console.log('Server is running on http://127.0.0.1:' + port);
+});
+*/
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/templates/dist/homepage.html'));
+});
+var server = https.createServer(credentials, app);
+server.listen(port, "foodcam.tk", function () {
+  console.log('running Web Server on' + port + 'port...');
 });
--- server.js
+++ server.js
@@ -1,20 +1,34 @@
-const express = require('express')
-const app = express()
-var path = require("path");
+var fs = require('fs');
+var https = require('https');
+var path = require('path');
+var express = require('express');
+var keyPath = '../ssl/private.key';
+var certPath = '../ssl/certificate.pem';
+var hskey = fs.readFileSync(keyPath);
+var hscert = fs.readFileSync(certPath);
+var app = express();
+var port = 8787;
 
-const hostname = '127.0.0.1';
-const port = 5000
+//var credentials = { key:hskey, cert:hscert};
 
-// release the limit of '/static' file
-app.use('/static', express.static(__dirname + '/static'));
-
-
+app.use('./static', express.static(__dirname + './static'));
+/*
 app.get('/', function (req, res) {
   console.log(`${__dirname+'/static'}`);
   res.sendFile(path.join(__dirname + '/templates/dist/homepage.html'));
 });
 
+app.listen(port, "127.0.0.1", () => {
+  console.log('Server is running on http://127.0.0.1:' + port);
+});
+*/
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/templates/dist/homepage.html')); 
+});
+
+var server = https.createServer(credentials,app);
+
+server.listen(port, "foodcam.tk", function() {
+  console.log('running Web Server on' + port + 'port...');
 });
