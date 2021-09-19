0bfb05b15af0facb4ff5951d176b16dcccd4e40f
morning <joeroy5376998@gmail.com>
2021-09-19 16:25:38 +0800

    fix server.js for local visit

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ var fs = require('fs');
 var https = require('https');
 var path = require('path');
 var express = require('express');
+/*
 var keyPath = '../ssl/private.key';
 var certPath = '../ssl/certificate.pem';
 var hskey = fs.readFileSync(keyPath);
 var hscert = fs.readFileSync(certPath);
+*/
 var app = express();
 var port = 8787;
 
-var credentials = { key:hskey, cert:hscert};
+app.use('/static', express.static(__dirname + '/static'));
 
-app.use('./static', express.static(__dirname + './static'));
-/*
 app.get('/', function (req, res) {
   console.log(`${__dirname+'/static'}`);
   res.sendFile(path.join(__dirname + '/templates/dist/homepage.html'));
@@ -21,7 +21,11 @@ app.get('/', function (req, res) {
 app.listen(port, "127.0.0.1", () => {
   console.log('Server is running on http://127.0.0.1:' + port);
 });
-*/
+
+/*
+var credentials = { key:hskey, cert:hscert};
+
+app.use('./static', express.static(__dirname + './static'));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/templates/dist/homepage.html')); 
@@ -30,5 +34,6 @@ app.get('/', function(req, res) {
 var server = https.createServer(credentials,app);
 
 server.listen(port, "foodcam.tk", function() {
-  console.log('running Web Server on' + port + 'port...');
+  console.log('Server is running on ' + port + ' port...');
 });
+*/
